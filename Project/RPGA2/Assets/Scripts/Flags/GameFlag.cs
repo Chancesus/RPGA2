0fc4a95e04b70427bb4568431dcb482a0a152461
Chancesus <chancekgsmith@gmail.com>
2022-09-10 16:56:08 -0700

    Started Added Bool Flag

---
+++ Project/RPGA2/Assets/Scripts/Flags/GameFlag.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Bool Game Flag")]
+public class GameFlag : ScriptableObject
+{
+    public bool Value;
+}
